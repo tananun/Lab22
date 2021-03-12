0dfb17829008929c1a2a7f984c84fe2fe315bfd5
tananun <tananun_c@cmu.ac.th>
2021-03-12 17:54:52 +0700

    F

--- lab22_1.cpp
+++ lab22_1.cpp
@@ -64,69 +64,19 @@ void List::append(int d){
 }
 
 //Write List::remove() here
-#include<iostream>
-using namespace std;
 
-
-class Node{
-    public:
-    	int data;
-    	Node *next;
-    	~Node();
-};
-
-class List{
-	public:
-		Node *root;
-		int size;
-		void show();
-		void append(int);
-		void insert(int,int);
-		void remove(int);
-};
-
-Node::~Node(){
-    cout << data << " was deleted.\n";
-}
-
-void List::insert(int d,int idx){	
-	Node *n = new Node;
-	n->data = d;
-	
-	if(idx == 0){
-		n->next = root;
-		root = n;
-		return;
-	}
+void List::remove(int d){
 	Node *current = root;
-	for(int i = 0; i < idx-1;i++){
-		current = current->next;
+	if (d == 0){
+		root = current->next;
+		delete current;
 	}
-	n->next = current->next;
-	current->next = n;		
-}
-
-void List::show(){
-	Node *current = root;
-	cout << current->data << " ";	
-	while(current->next){
-		current = current->next;
-		cout << current->data << " ";
-	}	
-}
-
-void List::append(int d){	
-	Node *n = new Node;
-	n->data = d; n->next = NULL;
-	if(root == NULL) root = n;
 	else{
-		Node *current = root;
-		while(current->next){
+		for (int i = 0; i < d-1; i++){
 			current = current->next;
 		}
-		current->next = n;
+	Node *rt = current->next->next;
+		delete current->next;
+		current->next = rt;
 	}
-	size++;
 }
-
-//Write List::remove() here
